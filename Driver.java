d1ff9c11c87d619559df19aef65a722f9da653dd
Shrutiman Mukherjee <88941689+ShrutimanMukherjee@users.noreply.github.com>
2022-12-06 03:11:44 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Driver
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1 --> Log in\n 2 --> Sign up");
+		int init_choice = sc.nextInt();
+		switch(choice)
+		{
+			case 1:
+				System.out.println("Enter User Id");
+			break;
+			
+			case 2:
+			
+			break;
+		}
+	}
+}
